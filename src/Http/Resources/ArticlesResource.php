9ce0e132032086b8bae3abcd857d51946800bb6c
Enrico Nardo <enr.nardo@gmail.com>
2019-03-01 17:38:05 +0100

    Improvement (I hope) of API resources

--- src/Http/Resources/ArticlesResource.php
+++ src/Http/Resources/ArticlesResource.php
@@ -2,8 +2,10 @@
 
 namespace Carpentree\Blog\Http\Resources;
 
-use Carpentree\Core\Http\Resources\CategoryResource;
+use Carpentree\Core\Http\Resources\Relationships\CategoryResourceRelationship;
+use Carpentree\Core\Http\Resources\Relationships\MetaResourceRelationship;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
 
 class ArticleResource extends JsonResource
 {
@@ -16,17 +18,37 @@ class ArticleResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'slug' => $this->slug,
-            'title' => $this->title,
-            'body' => $this->body,
-            'excerpt' => $this->excerpt,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
 
-            // Categories
-            'categories' => CategoryResource::collection($this->resource->categories)
+        return [
+            // Article field
+            'type' => 'articles',
+            'id' => $this->id,
+            'locale' => App::getLocale(),
+
+            'attributes' => [
+                'slug' => $this->slug,
+                'title' => $this->title,
+                'body' => $this->body,
+                'excerpt' => $this->excerpt,
+                'status' => $this->status,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+
+            'relationships' => [
+
+                // Categories
+                'categories' => $this->whenLoaded('categories', array(
+                    'data' => CategoryResourceRelationship::collection($this->categories)
+                ), array()),
+
+                // Meta
+                'meta' => $this->whenLoaded('meta', array(
+                    'data' => MetaResourceRelationship::collection($this->meta)
+                ), array())
+
+            ],
+
         ];
     }
 
