3b196500fe90c4e083b00ee14b1247171a9a487b
Enrico Nardo <enr.nardo@gmail.com>
2019-03-07 10:55:13 +0100

    Fix routes

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/admin')
     ->name('api.admin.')
-    ->namespace('Carpentree\Core\Http\Controllers\Admin')
+    ->namespace('Carpentree\Blog\Http\Controllers')
     ->group(function () {
 
     Route::middleware(['api', 'auth:api', 'verified', 'scope:admin'])
