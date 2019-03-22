542f271e6aca453bf8689e4c7af31032702f11f8
Enrico Nardo <enr.nardo@gmail.com>
2019-03-22 10:27:09 +0100

    Fix data access

---
+++ src/DataAccess/Article/Eloquent/EloquentArticleDataAccess.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Carpentree\Blog\DataAccess\Article\Eloquent;
+
+use Carpentree\Blog\DataAccess\Article\ArticleDataAccess;
+use Carpentree\Core\DataAccess\Eloquent\EloquentBaseDataAccess;
+
+class EloquentArticleDataAccess extends EloquentBaseDataAccess implements ArticleDataAccess
+{
+
+}
