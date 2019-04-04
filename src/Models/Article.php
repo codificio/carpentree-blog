ffcf3d27c59f4deb883ea8830bc61c9563592508
Enrico Nardo <enr.nardo@gmail.com>
2019-04-04 11:57:50 +0200

    Fix searchable Article

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -3,13 +3,13 @@
 namespace Carpentree\Blog\Models;
 
 use Carpentree\Blog\Models\Article\Translation;
+use Carpentree\Core\Scout\Searchable;
 use Carpentree\Core\Traits\Categorizable;
 use Carpentree\Core\Traits\HasMeta;
 use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Carpentree\Core\Scout\Searchable;
 use Plank\Mediable\Mediable;
 
 class Article extends Model
@@ -28,16 +28,6 @@ class Article extends Model
         'body',
         'excerpt'
     ];
-
-    /**
-     * Return true if you want to store this model in localized index.
-     *
-     * @return bool
-     */
-    public static function localizedSearchable()
-    {
-        return true;
-    }
 
     /**
      * Get the indexable data array for the model.
